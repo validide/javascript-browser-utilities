aab8d7543e3217c556895f7d83047c2c1f4e32f6
Valentin Dide <vali.dide@gmail.com>
2021-05-22 11:03:34 +0300

    Code

--- test/dom/form/index.spec.ts
+++ test/dom/form/index.spec.ts
@@ -1,7 +1,14 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
+
 import { test_appendformData } from './appendformData.spec';
 
 export function testFromModule() {
